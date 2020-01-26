0fa3161faa837cb84f3cd4ae8d225aa62212a629
Gyans92 <gyanprakash92@gmail.com>
2020-01-26 12:11:08 -0800

    merging

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import "./styles/navbar.css";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import TypoGraphy from "@material-ui/core/Typography";
 import AppBar from "@material-ui/core/AppBar";
 import { makeStyles } from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
-import TypoGraphy from "@material-ui/core/Typography";
 import Members from "./components/Members";
 import College from "./components/College";
 import Home from "./components/Home";
@@ -15,14 +18,33 @@ import { Switch, Route, Link, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/member" component={Members} />
-        <Route path="/college" component={College} />
-        <Route path="/home" component={Home} />
-        <Route path="/registration" component={Registration} />
-      </Switch>
-    </BrowserRouter>
+    <div class="main">
+      <AppBar color="" position="static">
+        <List component="nav">
+          <ListItem component="div">
+            <ListItemText inset>
+              <TypoGraphy color="inherit" variant="title">
+                Logo
+              </TypoGraphy>
+            </ListItemText>
+
+            <ListItemText inset>
+              <TypoGraphy color="inherit" variant="title">
+                QueerBook
+              </TypoGraphy>
+            </ListItemText>
+          </ListItem>
+        </List>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/member" component={Members} />
+            <Route path="/college" component={College} />
+            <Route path="/home" component={Home} />
+            <Route path="/registration" component={Registration} />
+          </Switch>
+        </BrowserRouter>
+      </AppBar>
+    </div>
   );
 }
 
--- frontend/src/components/Members.js
+++ frontend/src/components/Members.js
@@ -1,9 +1,17 @@
 import React from "react";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import TypoGraphy from "@material-ui/core/Typography";
-import AppBar from "@material-ui/core/AppBar";
+
+import { makeStyles } from "@material-ui/core/styles";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import InputLabel from "@material-ui/core/InputLabel";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import FormControl from "@material-ui/core/FormControl";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import InputAdornment from "@material-ui/core/InputAdornment";
 import axios from "axios";
 import { getQueryArg } from "../utils";
 import "./members.css";
@@ -31,52 +39,91 @@ class Members extends React.Component {
       });
     });
   };
+
+  handleChange = e => {
+    const currentMember = this.state.currentMember || [];
+    console.log(e, e.target);
+    currentMember.scraps.push(e.target.value);
+    this.setState({ currentMember });
+  };
+
   render() {
     const { currentMember, user } = this.state || {};
+    console.log(currentMember);
+    let { scraps } = currentMember || [];
+    // let scraps = [
+    //   "hello, You are so adorable!!",
+    //   "Like your hair!",
+    //   "Let's Catchup sometime"
+    // ];
+
+    const classes = {
+      card: {
+        maxWidth: 345
+      },
+      media: {
+        height: 500
+      }
+    };
     return (
       <div className="main">
-        <AppBar color="" position="static">
-          <List component="nav">
-            <ListItem component="div">
-              <ListItemText inset>
-                <TypoGraphy color="inherit" variant="title">
-                  Logo
-                </TypoGraphy>
-              </ListItemText>
-
-              <ListItemText inset>
-                <TypoGraphy color="inherit" variant="title">
-                  QueerBook
-                </TypoGraphy>
-              </ListItemText>
-            </ListItem>
-          </List>
-        </AppBar>
-        <div className="info">
-          <div className="profile-pic"></div>
-          <div className="profile-info">
-            <div className="Name">
-              <div>Name </div>
-              <div>{user.name}</div>
-            </div>
-            <div className="Email">
-              <div>Email </div> {user.email}
-            </div>
-            <div className="College">
-              <div>College</div>
-              {user.college}
-            </div>
-            <div className="Pronouns">
-              <div>Pronouns</div>
-              {user.pronouns}
-            </div>
-            <div className="IntersetedIn">
-              <div>Interseted in</div>
-              {user.interestedIn}
-            </div>
-          </div>
+        <div className="portfolio">
+          <Card className={classes.card}>
+            <CardActionArea>
+              {/* <CardMedia
+                className={classes.media}
+                image="https://images.spot.im/v1/production/yupgrpkcftvrhpc5jff5"
+                title="profile pic"
+              /> */}
+              <img src="https://www.billboard.com/files/styles/article_main_image/public/media/Kehlani-press-photo-2017-billboard-1548.jpg" />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {user.name}
+                </Typography>
+                <div></div>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {user.aboutme}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {user.college}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {user.pronouns}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {user.interestedIn}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button size="small" color="primary">
+                Like
+              </Button>
+              <Button size="small" color="primary">
+                connect
+              </Button>
+            </CardActions>
+          </Card>
         </div>
-        <div className="scrap"></div>
+        <div className="oldScraps">
+          {scraps && scraps.map(item => <div>{item}</div>)}
+        </div>
+        <div className="scrap">
+          <FormControl fullWidth className={classes.margin} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-amount"></InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-amount"
+              value={scraps && [scraps && scraps.length - 1]}
+              onChange={e => this.handleChange(e)}
+              startAdornment={
+                <InputAdornment position="start">
+                  Share your love with me...
+                </InputAdornment>
+              }
+              labelWidth={60}
+            />
+          </FormControl>
+        </div>
       </div>
     );
   }
