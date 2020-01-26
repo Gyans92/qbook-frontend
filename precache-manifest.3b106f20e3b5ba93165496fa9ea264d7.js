a8b1b66a33b0e2c6c8f83fc1eb871e5810ea9add
Gyans92 <gyanprakash92@gmail.com>
2020-01-26 13:15:18 -0800

    Updates

---
+++ precache-manifest.3b106f20e3b5ba93165496fa9ea264d7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c69e81ee3d62b1e08dd11bec00d33e47",
+    "url": "/qbook-frontend/index.html"
+  },
+  {
+    "revision": "fa95b7d9ebc274aaba0d",
+    "url": "/qbook-frontend/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "a7527c60702926516d6e",
+    "url": "/qbook-frontend/static/css/main.e3c0b863.chunk.css"
+  },
+  {
+    "revision": "fa95b7d9ebc274aaba0d",
+    "url": "/qbook-frontend/static/js/2.8101827d.chunk.js"
+  },
+  {
+    "revision": "755c3f280ab852eee3de9debeb376cd4",
+    "url": "/qbook-frontend/static/js/2.8101827d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a7527c60702926516d6e",
+    "url": "/qbook-frontend/static/js/main.d2e13ee0.chunk.js"
+  },
+  {
+    "revision": "e6ada02f0ea15f77c3f8",
+    "url": "/qbook-frontend/static/js/runtime-main.a806e3f9.js"
+  },
+  {
+    "revision": "56fde0b429990627df5136dde7da5b5a",
+    "url": "/qbook-frontend/static/media/WeX3su.56fde0b4.jpg"
+  },
+  {
+    "revision": "0025cab2ae08576c981219338838576a",
+    "url": "/qbook-frontend/static/media/lgbt_hug.0025cab2.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/qbook-frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
