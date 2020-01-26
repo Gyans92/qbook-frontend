902184425881f9d0bf150536a7acb6b95cd4d4c6
Gyans92 <gyanprakash92@gmail.com>
2020-01-26 08:59:44 -0800

    adding members data

--- frontend/src/components/Members.js
+++ frontend/src/components/Members.js
@@ -4,31 +4,82 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import TypoGraphy from "@material-ui/core/Typography";
 import AppBar from "@material-ui/core/AppBar";
+import axios from "axios";
+import { getQueryArg } from "../utils";
+import "./members.css";
 
-function Members(props) {
-  return (
-    <AppBar color="" position="static">
-      <List component="nav">
-        <ListItem component="div">
-          <ListItemText inset>
-            <TypoGraphy color="inherit" variant="title">
-              Logo
-            </TypoGraphy>
-          </ListItemText>
+class Members extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      college: [],
+      members: [],
+      currentMember: {},
+      user: {}
+    };
+  }
 
-          <ListItemText inset>
-            <TypoGraphy color="inherit" variant="title">
-              QueerBook
-            </TypoGraphy>
-          </ListItemText>
-        </ListItem>
-      </List>
-    </AppBar>
+  componentDidMount = () => {
+    axios.get("http://3.133.5.150:8080/search-user").then(res => {
+      let members = res.data || [];
+      let currentMember = members.find(a => a.email == getQueryArg("email"));
+      let user = members.find(a => a.email == getQueryArg("user"));
+      this.setState({
+        members: members,
+        currentMember: currentMember,
+        user: user
+      });
+    });
+  };
+  render() {
+    const { currentMember, user } = this.state || {};
+    return (
+      <div className="main">
+        <AppBar color="" position="static">
+          <List component="nav">
+            <ListItem component="div">
+              <ListItemText inset>
+                <TypoGraphy color="inherit" variant="title">
+                  Logo
+                </TypoGraphy>
+              </ListItemText>
 
-
-
-    
-  );
+              <ListItemText inset>
+                <TypoGraphy color="inherit" variant="title">
+                  QueerBook
+                </TypoGraphy>
+              </ListItemText>
+            </ListItem>
+          </List>
+        </AppBar>
+        <div className="info">
+          <div className="profile-pic"></div>
+          <div className="profile-info">
+            <div className="Name">
+              <div>Name </div>
+              <div>{user.name}</div>
+            </div>
+            <div className="Email">
+              <div>Email </div> {user.email}
+            </div>
+            <div className="College">
+              <div>College</div>
+              {user.college}
+            </div>
+            <div className="Pronouns">
+              <div>Pronouns</div>
+              {user.pronouns}
+            </div>
+            <div className="IntersetedIn">
+              <div>Interseted in</div>
+              {user.interestedIn}
+            </div>
+          </div>
+        </div>
+        <div className="scrap"></div>
+      </div>
+    );
+  }
 }
 
 export default Members;
